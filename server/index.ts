8e8f2bceb95be15662c146dc8e57eaa982add55a
Suhas Sriramaneni <suhaschoudary.s@gmail.com>
2025-09-04 13:10:46 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,17 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import cors from "cors";
+
 
 const app = express();
+app.use(
+  cors({
+    origin: "https://groupstudy-seven.vercel.app", 
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
